a3f3d0d244eca6a4f0c8e4c4de8a6fbff2017da5
Kimi4427 <89387392+Kimi4427@users.noreply.github.com>
2025-03-15 18:35:26 +0100

    Update main.js

--- download/main.js
+++ download/main.js
@@ -1,1 +1,14 @@
 
+    window.onloadTurnstileCallback = function () {
+  turnstile.render("#myWidget", {
+    sitekey: "0x4AAAAAABA5K1oArqzQYDyq",
+    callback: function (token) {
+      console.log(`Challenge Success ${token}`);
+        setTimeout((){
+            document.querySelector(".container").style.display = "flex";
+            document.getElementById("myWidget").style.display = none "none";
+        }, 2000);
+            
+    },
+  });
+};
