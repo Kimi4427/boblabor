c8af9f5bdefa9712e79c298aa0d73d9f2e696bf9
Kimi4427 <89387392+Kimi4427@users.noreply.github.com>
2025-03-15 18:56:19 +0100

    Update main.js

--- download/main.js
+++ download/main.js
@@ -1,41 +1,16 @@
 window.onloadTurnstileCallback = function () {
-turnstile.render("#myWidget", {
-window.onloadTurnstileCallback = function () {
- turnstile.render('#myWidget', {
- 
+  turnstile.render('#myWidget', {
     sitekey: "0x4AAAAAABA5K1oArqzQYDyq",
- 
     callback: function (token) {
- 
       console.log(`Challenge Success ${token}`);
- 
-
-        setTimeout((){
- 
-
-        setTimeout(function() {
- 
-           let container = document.querySelector(".container");
-        let widget = document.getElementById("myWidget");
-        
-        if (container && widget) {  // Sicherstellen, dass die Elemente existieren
-          container.style.display = "flex";
-          widget.style.display = "none";
-        } else {
-          console.log("Fehler: Ein Element wurde nicht gefunden.");
-        }
- 
-
-        }, 2000);
- 
-
-            
- 
-
-        }, 2000);  
- 
+      setTimeout(function() {
+        document.querySelector(".container").style.display = "flex";
+        document.getElementById("myWidget").style.display = "none";
+      }, 2000);  
     },
- 
+  });
+};
+
   });
  
 };
