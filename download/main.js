501c6b2e8a518e1daf378cec6f85c906c21476e4
Kimi4427 <89387392+Kimi4427@users.noreply.github.com>
2025-03-15 18:48:31 +0100

    Update main.js

--- download/main.js
+++ download/main.js
@@ -1,12 +1,19 @@
-window.onloadTurnstileCallback = function () {
+window.onload = function () { 
   turnstile.render('#myWidget', {
     sitekey: "0x4AAAAAABA5K1oArqzQYDyq",
     callback: function (token) {
       console.log(`Challenge Success ${token}`);
-        setTimeout(function() {
-            document.querySelector(".container").style.display = "flex";
-            document.getElementById("myWidget").style.display = "none";
-        }, 2000);  
+      setTimeout(function() {
+        let container = document.querySelector(".container");
+        let widget = document.getElementById("myWidget");
+        
+        if (container && widget) {  
+          container.style.display = "flex";
+          widget.style.display = "none";
+        } else {
+          console.log("Fehler: Ein Element wurde nicht gefunden.");
+        }
+      }, 2000);  
     },
   });
 };
